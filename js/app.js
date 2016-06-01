1cb47ff32f892414caa65ef3d89840cd1dae057a
kourosh mirzaei <Koory.yellow@gmail.com>
2016-05-31 20:39:41 -0400

    Rewriting the project!

---
+++ js/app.js
@@ -0,0 +1,128 @@
+
+
+var galleryItems =[
+{	
+"src":"01.jpg",
+"title":"Hay Bales",
+"alt":"I love hay bales. Took this snap on a drive through the countryside past some straw fields."
+},
+
+{	
+"src":"02.jpg",
+"title":"Lake",
+"alt":"The lake was so calm today. We had a great view of the snow on the mountains from here."
+},
+
+{	
+"src":"03.jpg",
+"title":"Canyon",
+"alt":"I hiked to the top of the mountain and got this picture of the canyon and trees below."
+},
+
+{	
+"src":"04.jpg",
+"title":"Iceberg",
+"alt":"It was amazing to see an iceberg up close, it was so cold but didn’t snow today."
+},
+
+{	
+"src":"05.jpg",
+"title":"Desert",
+"alt":"The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons."
+},
+
+{	
+"src":"06.jpg",
+"title":"Fall",
+"alt":"Fall is coming, I love when the leaves on the trees start to change color."
+},
+
+{	
+"src":"07.jpg",
+"title":"Plantation",
+"alt":"I drove past this plantation yesterday, everything is so green!"
+},
+
+{	
+"src":"08.jpg",
+"title":"Dunes",
+"alt":"My summer vacation to the Oregon Coast. I love the sandy dunes!"
+},
+
+{	
+"src":"09.jpg",
+"title":"Countryside Lane",
+"alt":"We enjoyed a quiet stroll down this countryside lane."
+},
+
+{	
+"src":"10.jpg",
+"title":"Sunset",
+"alt":"Sunset at the coast! The sky turned a lovely shade of orange."
+},
+
+{	
+"src":"11.jpg",
+"title":"Cave",
+"alt":"I did a tour of a cave today and the view of the landscape below was breathtaking."
+},
+
+{	
+"src":"12.jpg",
+"title":"Bluebells",
+"alt":"I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in."
+},
+
+{	
+"src":"13.jpg",
+"title":"Travel Love",
+"alt":"Top 10 of the most breathtaking places on this planet that you need to visit."
+},
+
+{	
+"src":"14.jpg",
+"title":"Hyper Travel",
+"alt":"This film is a HDR hyperlapse postcard that will take you to a journey through Georgia."
+},
+
+{	
+"src":"15.jpg",
+"title":"Travel",
+"alt":"A mix of memories from past years adventures for my travelblog on ✈ flysleepy.com."
+},
+
+{	
+"src":"16.jpg",
+"title":"NewYork Travel",
+"alt":"New York City is an international metropolis built on the shoulders of immigrants and their descendants."
+},
+
+]
+
+
+var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
